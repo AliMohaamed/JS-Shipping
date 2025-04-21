32a2d0220359844e2cb80d6aa3342dcaef0820ca
Ali Mohamed <alimohamed11907@gmail.com>
2025-04-21 15:38:19 +0200

    initnavbar logic

--- js/header.js
+++ js/header.js
@@ -5,4 +5,17 @@ fetch("./../components/header/header.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("header-container").innerHTML = data;
+    const navLinks = document.querySelector(".nav-container");
+    const menu = document.querySelector(".mobile-menu-toggle");
+    menu.addEventListener("click", () => {
+      let div = document.createElement("div");
+      div.classList.add("menu-responsive");
+      
+      div.append(navLinks);
+      menu.after(div);
+      console.log(div.parentElement);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
   });
