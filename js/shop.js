2af8780c08238da09ec5f09f5978ffa6f893f900
Ali Mohamed <alimohamed11907@gmail.com>
2025-04-25 19:41:58 +0300

    Add filters and some logic in UI

--- js/shop.js
+++ js/shop.js
@@ -1,1 +1,65 @@
 "use strict";
+
+import { getCategories, loadCategories } from "./dataLoader.js";
+
+document.addEventListener("DOMContentLoaded", async function () {
+  await loadCategories();
+  displayCategoriesHeader();
+  // Range
+  inputRange();
+  // Open Or Close filter title btn
+  openAndClose();
+  // Clear All
+  clearAll();
+});
+
+function displayCategoriesHeader() {
+  const categories = getCategories();
+  const categoryLabel = document.querySelector(".filter-options");
+
+  if (!categoryLabel) return;
+
+  categories.forEach((category) => {
+    const html = `<label class="filter-checkbox">
+                    <input type="checkbox" name="${category.slug}"/>
+                    <span>${category.name}</span>
+                    
+                  </label>`;
+    categoryLabel.insertAdjacentHTML("beforeend", html);
+  });
+}
+function openAndClose() {
+  const arrowButtons = document.querySelectorAll(".filter-title-btn");
+  const filterBottomAll = document.querySelectorAll(".filter-bottom");
+  arrowButtons.forEach((arrowButton)=>{
+    arrowButton.addEventListener("click", function () {
+      const filterBottom = arrowButton.nextElementSibling;
+      filterBottom.classList.toggle("active");
+      arrowButton.querySelector(".filter-arrow").textContent = filterBottom.classList.contains("active")
+        ? "↱"
+        : "↴";
+    });
+  })
+}
+
+// Range
+function inputRange() {
+  const rangeSlider = document.querySelector(".range-slider");
+  const priceInputStart = document.querySelector(".price-input-start");
+  const priceInputEnd = document.querySelector(".price-input-end");
+  rangeSlider.addEventListener("input", () => {
+    priceInputEnd.value = rangeSlider.value;
+  });
+}
+
+// clear all
+function clearAll(){
+    const clearBtn = document.querySelector('.clear-btn');
+    clearBtn.addEventListener('click',function(){
+        document.querySelector(".price-input-end").value = '999';
+        document.querySelector(".range-slider").value = '0';
+        document.querySelectorAll("input[type='checkbox']").forEach((check)=>{
+            check.checked = false
+        })
+    })
+}
